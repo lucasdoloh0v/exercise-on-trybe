56e258081888cdf79e48ab6e314082dacde26a36
lucasdoloh0v <lucasdolohov@gmail.com>
2021-08-25 19:24:58 -0300

    exemplo OR||

--- Fundamentos/B.4 Introdução à JavaScript e Lógica de Programação/Exemplos/operadores lógicos/scripts.js
+++ Fundamentos/B.4 Introdução à JavaScript e Lógica de Programação/Exemplos/operadores lógicos/scripts.js
@@ -1,3 +1,4 @@
+/// Operador And
 const currentHour = 15;
 let message = null;
 
@@ -14,3 +15,12 @@ if (currentHour >= 22) {
 }
 
 console.log(message)
+
+/// Operador OR
+let weekDay = "quarta-feira"
+
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" | weekDay === "sexta-feira") {
+  console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+} else {
+  console.log("FINALMENTE, descando merecido UwU")
+}
