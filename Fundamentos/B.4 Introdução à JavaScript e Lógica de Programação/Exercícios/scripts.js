ac4bf55bcc3c1c5e763e886a4d9ff6a1aa269292
lucasdoloh0v <lucasdolohov@gmail.com>
2021-08-26 09:14:10 -0300

    resolve o exercicio 2

--- Fundamentos/B.4 Introdução à JavaScript e Lógica de Programação/Exercícios/scripts.js
+++ Fundamentos/B.4 Introdução à JavaScript e Lógica de Programação/Exercícios/scripts.js
@@ -1,8 +1,19 @@
-const a = 8;
+/// 1°
+/*const a = 8;
 const b = 2;
 
 console.log(a + b);
 console.log(a - b);
 console.log(a * b);
 console.log(a / b);
-console.log(a % b);
+console.log(a % b);*/
+
+/// 2°
+const a = 2
+const b = 3
+
+if (a > b) {
+  console.log(a)
+} else {
+  console.log(b)
+}
