ac8b1a49d03852041c7e746ffb6dc078089aa2d8
lucasdoloh0v <lucasdolohov@gmail.com>
2021-08-26 09:19:12 -0300

    resolve o exercicio 3

--- Fundamentos/B.4 Introdução à JavaScript e Lógica de Programação/Exercícios/scripts.js
+++ Fundamentos/B.4 Introdução à JavaScript e Lógica de Programação/Exercícios/scripts.js
@@ -9,11 +9,24 @@ console.log(a / b);
 console.log(a % b);*/
 
 /// 2°
-const a = 2
+/* const a = 2
 const b = 3
 
 if (a > b) {
   console.log(a)
 } else {
   console.log(b)
+}*/
+
+/// 3°
+const a = 52
+const b = 74
+const c = 75
+
+if (a > b && a > c) {
+  console.log(a)
+} else if (b > a && b > c) {
+  console.log(b)
+} else {
+  console.log(c)
 }
