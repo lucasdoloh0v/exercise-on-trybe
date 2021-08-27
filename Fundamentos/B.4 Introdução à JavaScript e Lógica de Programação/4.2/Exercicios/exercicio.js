171a5874c9a13aa5751bd596a1cdd32b7cabd595
lucasdoloh0v <lucasdolohov@gmail.com>
2021-08-27 16:01:55 -0300

    resolve exercicio de fixação

--- Fundamentos/B.4 Introdução à JavaScript e Lógica de Programação/4.2/Exercicios/exercicio.js
+++ Fundamentos/B.4 Introdução à JavaScript e Lógica de Programação/4.2/Exercicios/exercicio.js
@@ -1,7 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //1
-console.log(numbers);
+for (var i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
 
 //2
 let sumNumbers = 0;
