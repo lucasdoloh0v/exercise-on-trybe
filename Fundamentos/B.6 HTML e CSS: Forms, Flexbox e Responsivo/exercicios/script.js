dfa3e370189e374245378fe4b6f69b3feaa8f823
Lucas Alves <lucasdolohov@gmail.com>
2021-09-15 19:09:05 -0300

    exercicio 1

---
+++ Fundamentos/B.6 HTML e CSS: Forms, Flexbox e Responsivo/exercicios/script.js
@@ -0,0 +1,12 @@
+const brazilStates = ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraiba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"];
+
+function addState(array) {
+  for(let i = 0; i < array.length; i += 1) {
+    const option = document.createElement("option");
+    const state = array[i];
+    option.innerText = state;
+    document.querySelector('#state').appendChild(option);
+  }
+}
+
+addState(brazilStates);
