8ae8d9c5bf225de50804873124fad6fcdb62fb8b
Lucas Alves <lucasdolohov@gmail.com>
2021-10-28 08:52:56 -0300

    para fixar

---
+++ Desenvolvimento-Front-End/B.10 Introducao a React/10.1/para-fixar/helloworld-react/src/Component.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Infos extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Lucas Alves da Silva</h1>
+        <p>Estou começando meus estudos em React e esse é um exercício introdutório</p>
+      </>
+    )
+  }
+}
+
+export default Infos;
